a7fa1f2e3f41a446ae6209adef90e053a3ef7816
lvjianshu <1020094189@qq.com>
2019-12-06 16:31:32 +0800

    1206

--- cli.py
+++ cli.py
@@ -17,7 +17,12 @@ def genGGA(hhddss):
         GGA = '$GPGGA,' + str(hhddss) + ',' + str(B) + ',N,' + str(L) + ',E,1,24,0.6,43.580,M,-6.251,M,,*47'
         yield GGA
 
-async def connect_cors(cb):
+async def rabbit(Msg):
+    channel.basic_publish(exchange=ex, routing_key='cc', body=Msg)
+
+
+
+async def connect_cors():
     connect = asyncio.open_connection(host, port)
     reader, writer = await connect
     EncryptionStr = b64encode(str.encode(user + ':' + password))
@@ -43,7 +48,7 @@ async def connect_cors(cb):
             Msg = await reader.read(1500)
             Msg = b2a_hex(Msg).decode('utf-8')
             # 打印差分数据，根据需要选择是否屏蔽
-            channel.basic_publish(exchange=ex, routing_key='cc', body=Msg)
+            await rabbit(Msg)
             await asyncio.sleep(1)
 
 
@@ -59,14 +64,15 @@ if __name__ == '__main__':
     mountpoint = cf.get('ntripcaster', 'mountpoint')
     locaion_range = float(cf.get('client', 'range'))
     simulator_number = int(cf.get('client', 'clientnumber'))
-
     credentials = pika.PlainCredentials(cf.get('rabbitmq', 'user'), cf.get('rabbitmq', 'password'))
     connection = pika.BlockingConnection(pika.ConnectionParameters(host=cf.get('rabbitmq', 'host'), port=int(cf.get('rabbitmq', 'port')), credentials=credentials))
     channel = connection.channel()
     ex = cf.get('rabbitmq', 'exchange')
     channel.exchange_declare(exchange=ex, exchange_type='fanout')
 
+
+
     # 定义事件循环
-    task = [connect_cors(i) for i in range(simulator_number)]
+    task = [connect_cors() for i in range(simulator_number)]
     loop = asyncio.get_event_loop()
     loop.run_until_complete(asyncio.wait(task))
